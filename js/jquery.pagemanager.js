1219ec30924d87b8451f1422803c490352cf20fd
docmars <docmars@gmail.com>
2013-06-21 16:55:28 -0600

    Added animation.
    Animations, options, automatic height animation.

--- js/jquery.pagemanager.js
+++ js/jquery.pagemanager.js
@@ -7,18 +7,41 @@
 	var options = {
 		validate: false,
 		cache: true,
-		container: "#pagemanager"
+		container: "#pagemanager",
+		firstPage: '',
+
+		animate: true,
+		animationProperties: {
+			before: { opacity: 0 },
+			after: { opacity: 1 }
+		},
+		animationDuration: {
+			before: 100,
+			after: 500
+		},
+		animationEasing: {
+			before: 'swing',
+			after: 'swing'
+		},
+		animateHeight: true,
+		animationHeightDuration: 250,
+		animationHeightEasing: 'swing'
 	};
 
-	var cache = {};
-	var states = {
-		last: null,
-		current: null
-	};
+	var cache = {},
+		states = {
+			last: null,
+			current: null
+		},
+		$container,
+		$data;
 
 	var pub = {
 
-		load: function(url) {
+		// Public methods for use with constructor.
+		// Call with a string, arguments second.
+		 
+		goto: function(url) {
 			_show(url);
 		}
 
@@ -26,39 +49,86 @@
 
 	function _init( opts ) {
 		$.extend(options, opts || {});
-
+		$container = $(options.container);
+		options.animateHeight ? $container.css('overflow', 'hidden') : null;
+		_show(options.firstPage);
 	}
 
-	function _show( state ) {
+	function _show( url ) {
 		if ( states.current ) {
 			_saveState( states.current );
 			states.last = states.current;
 		}
 
-		if ( cache[state] ) {
-			log(state + " exists!");
-			_restoreState( state );
+		states.current = url;
+
+		if ( _isThisStateCached() ) {
+			_restoreState();
 		} else {
-			log("Rendering page: " + state);
-			_renderState( state );
+			_newState();
 		}
 	}
 
-	function _renderState(url) {
-		var _url = url + '.html';
+	// Check if current state is cached.
+	function _isThisStateCached() {
+		return (options.cache && cache[states.current]);
+	}
+
+	// Check if passed state is cached.
+	function _isStateCached( state ) {
+		return (options.cache && cache[state]);
+	}
+
+	// Cache a state OR overwrite existing cache entry for a page.
+	function _saveState( state ) {
+		cache[state] = $container.html();
+	}
+
+	// Create brand new state + make request to grab page DOM data.
+	function _newState() {
+		var _url = states.current + '.html';
 		$.get(_url, function(data) {
-			$(options.container).html(data);
-			states.current = url;
+			$data = data;
+			options.animate ? _renderAnimate() : _render();
 		});
 	}
 
-	function _saveState( state ) {
-		cache[state] = $(options.container).html();
+	// Restore cached state with DOM data.
+	function _restoreState( state ) {
+		options.animate ? _renderAnimate( cache[state] ) : _render( cache[state] );
 	}
 
-	function _restoreState( state ) {
-		$(options.container).html( cache[state] );
-		states.current = state;
+	// Used to be animateHeight for cached entries:
+	function _renderAnimate() {
+		_animateBefore( function() {
+			if ( options.animateHeight ) {
+				var oldHeight = $container.height();
+				_isThisStateCached() ? $container.html(cache[states.current]) : $container.html($data);
+				$container.css('height', 'auto');
+				var newHeight = $container.height();
+				$container.height(oldHeight).animate( { 'height':newHeight }, { duration: options.animationHeightDuration, easing: options.animationHeightEasing, 
+					complete: function() { 
+						$container.css('height', 'auto');
+						_animateAfter();
+					}
+				});
+			} else {
+				_isThisStateCached() ? $container.html(cache[states.current]) : $container.html($data);
+				_animateAfter();
+			}
+		});
+	}
+
+	function _render(data) {
+		_isThisStateCached() ? $container.html(cache[states.current]) : $container.html($data);
+	}
+
+	function _animateBefore( complete ) {
+		$container.animate(options.animationProperties.before, {duration: options.animationDuration.before, easing: options.animationEasing.before, complete: complete, queue: false});
+	}
+
+	function _animateAfter( complete ) {
+		$container.animate(options.animationProperties.after, {duration: options.animationDuration.after, easing: options.animationEasing.after, complete: complete, queue: false});
 	}
 
 	$.pageManager = function(method) {
--- js/main.js
+++ js/main.js
@@ -4,17 +4,29 @@ log = function(msg) {
 
 jQuery(document).ready(function($) {
 	
-		$.pageManager('load', 'partials/test-1');
+		$.pageManager({
+			firstPage: 'partials/test-1',
+			animate: true,
+			animateHeight: true
+		});
 
 		$('body').on('click', '.test-1-link', function(event) {
 			event.preventDefault();
 			var url = $(this).data('url');
-			$.pageManager('load', url);
+			$.pageManager('goto', url);
 		});
 
 		$('body').on('click', '.add-element', function(event) {
 			event.preventDefault();
 			$('#pagemanager').append("<h2>This was added later.</h2>");
+		});
+
+		var $h2 = $('h2');
+
+		$('h2').animate({
+			opacity: 0
+		},{duration: 2000}).promise().then(function(){
+			log("Done");
 		});
 
 });
